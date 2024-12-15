6bea3b43dbe866c703d7bc9a4a9f7421164bcff8
Muhammad Ridho <reedho@gmail.com>
2024-12-15 17:29:51 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div className="m-4 space-y-4">
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
